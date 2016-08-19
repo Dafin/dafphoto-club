85b2ed45ae7e358689f01110b9dcbc2ebe111c9c
Dafin <dafin@users.noreply.github.com>
2016-08-20 00:28:05 +0100

    Fixed typo in photo model Fixed relationships in user model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
-reloadclass Photo < ActiveRecord::Base
-  belongs_to :users
+class Photo < ActiveRecord::Base
+  belongs_to :user
   has_many :likes #redundant just for counting? join table used ? caching?
-  has_many :users_who_liked, through: :likes, class_name: "User", foreign_key: :user_id
+  has_many :users_who_liked, through: :likes, class_name: "User", foreign_key: :user_id, source: :user
 
   # mount_uploader :image_uri, ImageUploader
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :photos 
   has_many :likes
-  has_many :liked_photos, through: :likes, class_name: "Photo", source: :photo
+  has_many :liked_photos, through: :likes, class_name: "Photo", foreign_key: :photo_id, source: :photo
 end
