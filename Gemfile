a4c94d535fc9c0dd7cebb46eb70b2a0901491963
Dafin <dafin@users.noreply.github.com>
2016-08-20 19:20:56 +0100

    Small change to Gemfile, added required:false to photo model to allow photos existing without having any likes

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem 'simplecov'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'rspec'
+  gem 'rspec-rails'
   gem 'pry-byebug'
 end
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :likes #redundant just for counting? join table used ? caching?
-  has_many :users_who_liked, through: :likes, class_name: "User", foreign_key: :user_id, source: :user
+  has_many :users_who_liked, through: :likes, class_name: "User", foreign_key: :user_id, source: :user, required:false
 
   # mount_uploader :image_uri, ImageUploader
 
